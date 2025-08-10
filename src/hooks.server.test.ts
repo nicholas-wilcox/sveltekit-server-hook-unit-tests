b899420c0ad6ff747544b0c9d760a795da9e4e6c
Nicholas Wilcox <nwilcox@linicks.dev>
2025-08-10 10:15:27 -0500

    Add unit tests for server hooks

---
+++ src/hooks.server.test.ts
@@ -0,0 +1,47 @@
+import { describe, expect, it } from 'vitest';
+import { redirectHook, errorHook } from './hooks.server';
+import { mockHandleParams } from '$lib/mocks/request';
+
+describe('hooks', () => {
+	describe('redirectHook', () => {
+		it('redirects', async () => {
+			let { event, resolve } = mockHandleParams();
+			await expect(redirectHook({ event, resolve })).rejects.toThrowRedirect();
+		});
+
+		it('returns a 302 status', async () => {
+			let { event, resolve } = mockHandleParams();
+			await expect(redirectHook({ event, resolve })).rejects.toThrowRedirect({
+				status: 302
+			});
+		});
+
+		it('redirects to the login route', async () => {
+			let { event, resolve } = mockHandleParams();
+			await expect(redirectHook({ event, resolve })).rejects.toThrowRedirect({
+				location: '/login'
+			});
+		});
+	});
+
+	describe('errorHook', () => {
+		it('errors', async () => {
+			let { event, resolve } = mockHandleParams();
+			await expect(errorHook({ event, resolve })).rejects.toThrowHttpError();
+		});
+
+		it('returns a 500 status', async () => {
+			let { event, resolve } = mockHandleParams();
+			await expect(errorHook({ event, resolve })).rejects.toThrowHttpError({
+				status: 500
+			});
+		});
+
+		it('returns an error message', async () => {
+			let { event, resolve } = mockHandleParams();
+			await expect(errorHook({ event, resolve })).rejects.toThrowHttpError({
+				message: 'Server Error'
+			});
+		});
+	});
+});
