7ba10c7279ec0059038b3979cf85a299b61ca543
Nicholas Wilcox <nwilcox@linicks.dev>
2025-08-10 09:40:36 -0500

    Add server hooks

---
+++ src/hooks.server.ts
@@ -0,0 +1,14 @@
+/**
+ * This module contains simple server hooks that use SvelteKit's utility
+ * functions for managing redirects and expected server errors.
+ */
+import type { Handle } from '@sveltejs/kit';
+import { redirect, error } from '@sveltejs/kit';
+
+export const redirectHook = (async ({ event, resolve }) => {
+	redirect(302, '/login');
+}) satisfies Handle;
+
+export const errorHook = (async ({ event, resolve }) => {
+	error(500, 'Server Error');
+}) satisfies Handle;
